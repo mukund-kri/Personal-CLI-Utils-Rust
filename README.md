f991402cba42774c57aa7b2e5cae1ec686562466
Mukund K <mukund.kri@gmail.com>
2025-01-18 22:13:01 +0530

    Finished new_note

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Personal-CLI-Utils-Rust
+
+In this project I am trying to migrate all my personal CLI utilities, from mostly
+Python to Rust. 
+
+It's vanity project, the python version works great, I just want to do it in Rust.
+
+## Utilities
+
+### `new_note`
+A utility to create notes on a particular topic. I mostly use it with
+`100 days of programming X` challenge.
+
+**Usage**
+```bash
+new_note <topic>
+```
+
+where `<topic>` is the topic of the note in snake_case.
+
+## Installation
+
+In the cargo root directory, run the following command:
+
+```bash
+cargo install --path .
+```
--- src/bin/new_note.rs
+++ src/bin/new_note.rs
@@ -1,3 +1,34 @@
+use std::fs::create_dir;
+
+use chrono::prelude::*;
+use clap::Parser;
+use convert_case::{Case, Casing};
+
+// Command line arguments
+#[derive(Parser)]
+struct Cli {
+    // The only thing I need is the title of the note
+    title: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // get today's date and format so that I can prefix to the note directory
+    let today = Local::now();
+    let today_str = today.format("%Y-%m-%d").to_string();
+
+    // Parse the command line arguments
+    let args = Cli::parse();
+
+    // Create the note directory
+    let note_dir = format!("{}-{}", today_str, args.title);
+    create_dir(&note_dir).expect("Failed to create note directory");
+
+    // The notes in markdown format
+    let note_md = format!("{}/{}.md", &note_dir, args.title,);
+    // Touch the note file
+    std::fs::File::create(&note_md).expect("Failed to create note file");
+
+    // Next, create scala code file
+    let code_file = format!("{}/{}.scala", &note_dir, args.title.to_case(Case::Pascal));
+    std::fs::File::create(&code_file).expect("Failed to create code file");
 }
